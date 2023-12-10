a6fba180bc0284bdaa3990e1009b06e8938da074
movabe <121778000+movabe@users.noreply.github.com>
2023-12-09 19:41:08 -0500

    Update frequency-bars.js

--- app/frequency-bars.js
+++ app/frequency-bars.js
@@ -7,7 +7,7 @@
 const FrequencyBars = function (selector) {
   this.$canvas = document.querySelector(selector);
   this.$canvas.width = document.body.clientWidth;
-  this.$canvas.height = document.body.clientHeight / 2;
+  this.$canvas.height = document.body.clientHeight / 4;
   this.canvasContext = this.$canvas.getContext("2d");
 };
 
@@ -15,7 +15,7 @@ const FrequencyBars = function (selector) {
  * @param {Uint8Array} data
  */
 FrequencyBars.prototype.update = function (data) {
-  const length = 32; // low frequency only
+  const length = 64; // low frequency only
   const width = this.$canvas.width / length - 0.5;
   this.canvasContext.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
   for (var i = 0; i < length; i += 1) {
