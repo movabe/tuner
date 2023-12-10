3a6dc95ec2cffeb8d14fe0a651e8d62057475575
movabe <121778000+movabe@users.noreply.github.com>
2023-12-10 14:39:48 -0500

    Update color-change.js

--- app/color-change.js
+++ app/color-change.js
@@ -1,47 +1,36 @@
-var refreshTime = 100; //microseconds
+var refreshTime = 100; // microseconds
 var sensitivity = 2; // + and - interval
 
 function colorChange() {
-  var el = document.getElementById("meter-pointer");
+  var el = document.querySelector(".tuner-container .meter-pointer");
   var st = window.getComputedStyle(el, null);
-  var tr = st.getPropertyValue("-webkit-transform") ||
-    st.getPropertyValue("-moz-transform") ||
-    st.getPropertyValue("-ms-transform") ||
-    st.getPropertyValue("-o-transform") ||
-    st.getPropertyValue("transform") ||
-    "FAIL";
+  var tr = st.getPropertyValue("transform") || "FAIL";
 
   var values = tr.split('(')[1].split(')')[0].split(',');
   var a = values[0];
   var b = values[1];
-  var c = values[2];
-  var d = values[3];
-
   var scale = Math.sqrt(a * a + b * b);
   var sin = b / scale;
   var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
 
-  // Find elements within .tuner-container
+  // Apply styles directly to the elements
   var tunerContainer = document.querySelector('.tuner-container');
-  
-  // Find .plus, .minus, and .color-change within tunerContainer
+  tunerContainer.style.backgroundColor = '';  // Reset background color
+
   var plusElement = tunerContainer.querySelector('.plus');
   var minusElement = tunerContainer.querySelector('.minus');
-  var colorChangeElement = tunerContainer.querySelector('.color-change');
 
   if (angle >= -sensitivity && angle <= sensitivity) {
     plusElement.style.color = 'green';
     minusElement.style.color = 'green';
-    colorChangeElement.style.backgroundColor = 'green';
   } else if (angle < -sensitivity) {
     plusElement.style.color = 'gray';
     minusElement.style.color = 'red';
-    colorChangeElement.style.color = '#748291';
-    colorChangeElement.style.backgroundColor = '#748291';
+    tunerContainer.style.backgroundColor = '#748291';
   } else if (angle > sensitivity) {
     plusElement.style.color = 'red';
     minusElement.style.color = 'gray';
-    colorChangeElement.style.backgroundColor = '#748291';
+    tunerContainer.style.backgroundColor = '#748291';
   }
 }
 
