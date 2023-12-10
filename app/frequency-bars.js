35ee20e395f48559e568ec6d73896855082e54d0
movabe <121778000+movabe@users.noreply.github.com>
2023-12-10 15:22:44 -0500

    Update frequency-bars.js

--- app/frequency-bars.js
+++ app/frequency-bars.js
@@ -19,7 +19,7 @@ FrequencyBars.prototype.update = function (data) {
   const width = this.$canvas.width / length - 0.5;
   this.canvasContext.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
   for (var i = 0; i < length; i += 1) {
-    this.canvasContext.fillStyle = "#cccccc";
+    this.canvasContext.fillStyle = "#c5d4d8";
     this.canvasContext.fillRect(
       i * (width + 0.5),
       this.$canvas.height - data[i] / 2,
