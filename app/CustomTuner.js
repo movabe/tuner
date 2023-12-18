eff87a7c15e56014dd45b8af4b95056a662fb488
movabe <121778000+movabe@users.noreply.github.com>
2023-12-18 11:40:13 -0500

    Create CustomTuner.js

---
+++ app/CustomTuner.js
@@ -0,0 +1,24 @@
+class CustomTuner extends Tuner {
+  play(frequency, volume) {
+    if (!this.oscillator) {
+      this.oscillator = this.audioContext.createOscillator();
+      this.gainNode = this.audioContext.createGain();
+
+      this.oscillator.connect(this.gainNode);
+      this.gainNode.connect(this.audioContext.destination);
+      this.oscillator.start();
+    }
+    
+    this.oscillator.frequency.value = frequency;
+    this.gainNode.gain.value = volume;
+  }
+    
+  stopOscillator() {
+    if (this.oscillator) {
+      this.oscillator.disconnect(this.gainNode);
+      this.gainNode.disconnect(this.audioContext.destination);
+      this.oscillator.stop();
+      this.oscillator = null;
+    }
+  }
+}
